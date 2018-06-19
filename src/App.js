f117561f43fb5d92e708058c315eb7a884666c80
sitneva <sitneva.t.v@gmail.com>
2018-06-19 15:28:10 +0300

    config for burger builder app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
